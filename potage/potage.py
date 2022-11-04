dee821f4e7c569f414c9855b7960d5995fe9a606
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2022-11-04 16:27:32 +0900

    Implement potage.yaml loading part

--- potage/potage.py
+++ potage/potage.py
@@ -1,14 +1,11 @@
 import argparse
 import os
 import dataclasses
+import pkg_resources
+import yaml
 
-@dataclasses.dataclass
-class Config:
-    title: str
-    author: str
-    input_dir: str
-    output_dir: str
-    date_format: str
+CONFIG_FILE_NAME = "potage.yaml"
+
 
 @dataclasses.dataclass
 class MarkDownFile:
@@ -17,18 +14,26 @@ class MarkDownFile:
     created_at: str
     edited_at: str
 
-def parse_command_line_args():
-    # Parse potage command's arguments.
-    # no argument: Try to convert all MarkDown files.
-    # --version argument: Show potage package version.
-    # --help argument: Show usage.
-    pass
 
-def parse_config(path):
-    # Parse potage.yaml and return Config dataclass.
-    # If some of the options are missing, raise a exception.
-    # To check required parameters, please read Config dataclass definition.
-    pass
+def parse_command_line_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="potage: Minimal Static Site Generator."
+    )
+    parser.add_argument("--version", action="store_true", help="Show potage version.")
+    args = parser.parse_args()
+    return args
+
+
+def print_version():
+    version = pkg_resources.get_distribution("potage").version
+    print("potage version: " + version)
+
+
+def parse_config() -> dict:
+    with open(CONFIG_FILE_NAME) as file:
+        config = yaml.safe_load(file)
+    return config
+
 
 def load_template():
     # Load jinja2 template files.
@@ -36,18 +41,28 @@ def load_template():
     # pages.html is a general page template.
     pass
 
+
 def load_markdown_files(input_dir):
     # Load files include input_dir recursively.
     # Retuen a list of MarkDown instances.
     pass
 
+
 def convert_index(markdown):
     # Convert index.md's MarkDown instance to index.html.
     pass
+
 
 def convert_page(markdown):
     # Convert a MarkDown instance into page.html
     pass
 
+
 def main():
-    print("Hello, potage!")
+    args = parse_command_line_args()
+    if args.version == True:
+        print_version()
+    else:
+        print("Convert All MarkDown files")
+        config = parse_config()
+        print(config)
